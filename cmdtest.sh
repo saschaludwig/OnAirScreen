5229347f58b2e9902f40fa5174d0620e018691ad
Sascha Ludwig <sascha@rc5.de>
2012-10-22 01:20:51 +0200

    added basic remote config functionality

--- cmdtest.sh
+++ cmdtest.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo  "CONF:LED1TEXT=FOO" | nc -w 1 -u 127.0.0.1 3310
+echo  "CONF:LED2TEXT=FOO" | nc -w 1 -u 127.0.0.1 3310
+echo  "CONF:LED3TEXT=FOO" | nc -w 1 -u 127.0.0.1 3310
+echo  "CONF:LED4TEXT=FOO" | nc -w 1 -u 127.0.0.1 3310
+echo  "CONF:STATIONNAME=BAR" | nc -w 1 -u 127.0.0.1 3310
+echo  "CONF:SLOGAN=Generic 2342" | nc -w 1 -u 127.0.0.1 3310
+
 echo  "NOW:The Testers - Test around the clock" | nc -w 1 -u 127.0.0.1 3310
 echo  "NEXT:coming up next: the foo the bar and the generic" | nc -w 1 -u 127.0.0.1 3310
 
@@ -15,3 +22,12 @@ echo "LED3:OFF" | nc -w 1 -u 127.0.0.1 3310
 echo "LED4:ON" | nc -w 1 -u 127.0.0.1 3310
 echo "LED4:OFF" | nc -w 1 -u 127.0.0.1 3310
 
+
+echo -e "LED1:ON\nLED2:ON\nLED3:ON\nLED4:ON" | nc -w 1 -u 127.0.0.1 3310
+exit
+conf="LED1TEXT=ON AIR\n
+LED2TEXT=ATTENTION\n
+LED3TEXT=DOORBELL\n
+LED4TEXT=PHONE\n
+"
+echo -e "CONF:$conf" | nc -w 1 -u 127.0.0.1 3310
