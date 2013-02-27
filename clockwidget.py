6c5102d5171f848170729ed4e78f8f437d4c16ba
Sascha Ludwig <sascha@radio42.de>
2013-02-27 15:19:22 +0100

    changed to BSD 2-clause license and added timer icon

--- clockwidget.py
+++ clockwidget.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 #############################################################################
 ##
 ## OnAirScreen Analog / Digital Clock implementation
-## Copyright (C) 2012 Sascha Ludwig
+## Copyright (C) 2013 Sascha Ludwig
 ## All rights reserved.
 ##
-## contains code from Riverbank Computing Limited and Nokia Corporation
-## for details: see copyright notice below
+## start.py
+## This file is part of OnAirScreen
+##
+## You may use this file under the terms of the BSD license as follows:
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+##   * Redistributions of source code must retain the above copyright
+##     notice, this list of conditions and the following disclaimer.
+##   * Redistributions in binary form must reproduce the above copyright
+##     notice, this list of conditions and the following disclaimer in
+##     the documentation and/or other materials provided with the
+##     distribution.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+## this file contains code from Riverbank Computing Limited
+## and Nokia Corporation for details: see copyright notice below
 ##
 
 #############################################################################
