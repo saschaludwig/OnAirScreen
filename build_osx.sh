0384e7cc77a9fbb0403252dd5f73beb0b82060c3
Sascha Ludwig <sascha@rc5.de>
2019-01-12 23:42:16 +0100

    some changes to build scripts, retina support for OSX

--- build_osx.sh
+++ build_osx.sh
@@ -1,1 +1,3 @@
-pyinstaller -F -y -w -i images/oas_icon.icns -n OnAirScreen "$@" start.py
+#!/usr/bin/env bash
+#pyinstaller -F -y -w -i images/oas_icon.icns -n OnAirScreen --distpath ./dist/osx/ "$@" start.py
+pyinstaller --distpath ./dist/osx/ OnAirScreen_osx.spec
