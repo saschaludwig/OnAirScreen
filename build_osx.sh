f63f4f893532a7f791dddb6c20138a834b0b1716
Sascha Ludwig <sascha@rc5.de>
2019-01-12 23:56:13 +0100

    add short hash to dist binary

--- build_osx.sh
+++ build_osx.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 #pyinstaller -F -y -w -i images/oas_icon.icns -n OnAirScreen --distpath ./dist/osx/ "$@" start.py
-pyinstaller --distpath ./dist/osx/ OnAirScreen_osx.spec
+pyinstaller -y --distpath ./dist/osx/ "$@" OnAirScreen_osx.spec
+mv -f ./dist/osx/OnAirScreen.app ./dist/osx/OnAirScreen_$(git rev-parse --short HEAD).app
