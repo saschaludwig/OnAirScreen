5727746b47f340fe8c4bba19f601c6cbb3189ad2
Sascha Ludwig <sascha@rc5.de>
2019-01-12 22:51:31 +0100

    added buildscripts

---
+++ build_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pip install -r requirements.txt && pyinstaller -F -y -w --clean -i images/oas_icon.ico --dist ./dist/windows --workpath /tmp --specpath /tmp -n OnAirScreen start.py"
