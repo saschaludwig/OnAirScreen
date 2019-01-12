bcb8c3b5e1481fb48dbf7753fb649c2e795568d4
Sascha Ludwig <sascha@rc5.de>
2019-01-12 23:56:13 +0100

    add short hash to dist binary

--- build_win.sh
+++ build_win.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 eval $(docker-machine env default)
-docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pip install -r requirements.txt && pyinstaller -F -y -w --clean -i images/oas_icon.ico --dist ./dist/windows --workpath /tmp --specpath /tmp -n OnAirScreen start.py"
+docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pip install -r requirements.txt && pyinstaller -F -y -w --clean -i images/oas_icon.ico --dist ./dist/win --workpath /tmp --specpath /tmp -n OnAirScreen start.py"
+mv -f ./dist/win/OnAirScreen.exe ./dist/win/OnAirScreen_$(git rev-parse --short HEAD).exe
