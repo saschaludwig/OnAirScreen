093fbbe5398c101989966dc3829df5d48e2d6108
Sascha Ludwig <sascha@rc5.de>
2019-01-12 23:42:16 +0100

    some changes to build scripts, retina support for OSX

--- build_win.sh
+++ build_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+eval $(docker-machine env default)
 docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pip install -r requirements.txt && pyinstaller -F -y -w --clean -i images/oas_icon.ico --dist ./dist/windows --workpath /tmp --specpath /tmp -n OnAirScreen start.py"
