6be86bf5407a9f65d5fa2ca3b6be48004fa80f5f
Sascha Ludwig <sascha@rc5.de>
2019-01-20 00:43:25 +0100

    all kind of bugfixes, added configurable date format

--- build_osx.sh
+++ build_osx.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 #pyinstaller -F -y -w -i images/oas_icon.icns -n OnAirScreen --distpath ./dist/osx/ "$@" start.py
-pyinstaller -y --distpath ./dist/osx/ "$@" OnAirScreen_osx.spec
-mv -f ./dist/osx/OnAirScreen.app ./dist/osx/OnAirScreen_$(git rev-parse --short HEAD).app
+rm -rf ./dist/osx/*
+make && \
+pyinstaller -y --distpath ./dist/osx/ "$@" OnAirScreen_osx.spec && \
+mv -f ./dist/osx/OnAirScreen.app ./dist/osx/OnAirScreen_$(git rev-parse --short HEAD)_$(git describe --abbrev=0 --tags).app
+
+open dist/osx/
--- build_win.sh
+++ build_win.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+rm -rf ./dist/win/*
+make && \
+eval $(docker-machine env default) && \
+docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pip install -r requirements.txt && pyinstaller -F -y -w --clean -i images/oas_icon.ico --dist ./dist/win --workpath /tmp --specpath /tmp -n OnAirScreen start.py" && \
+mv -f ./dist/win/OnAirScreen.exe ./dist/win/OnAirScreen_$(git rev-parse --short HEAD)_$(git describe --abbrev=0 --tags).exe
 
-eval $(docker-machine env default)
-docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pip install -r requirements.txt && pyinstaller -F -y -w --clean -i images/oas_icon.ico --dist ./dist/win --workpath /tmp --specpath /tmp -n OnAirScreen start.py"
-mv -f ./dist/win/OnAirScreen.exe ./dist/win/OnAirScreen_$(git rev-parse --short HEAD).exe
+open dist/win/
