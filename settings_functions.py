bd3dbbfc7f2d3fec89fc1926fe1d82a01e2d2632
Sascha Ludwig <sascha@rc5.de>
2019-02-03 23:17:39 +0100

    0.9beta2

--- settings_functions.py
+++ settings_functions.py
@@ -42,7 +42,7 @@ from settings import Ui_Settings
 from collections import defaultdict
 import json
 
-versionString = "0.9beta1"
+versionString = "0.9beta2"
 
 
 # class OASSettings for use from OAC
