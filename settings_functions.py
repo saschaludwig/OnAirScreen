b1ac9e0ad78f2371ae55acbf27055c53ba3aee6e
Sascha Ludwig <sascha.ludwig@dienes.de>
2019-06-11 16:33:18 +0200

    changed default language in demo widget

--- settings_functions.py
+++ settings_functions.py
@@ -44,7 +44,7 @@ import json
 
 versionString = "0.9.1beta2"
 weatherWidgetFallback = """
-<a class="weatherwidget-io" href="https://forecast7.com/de/50d777d19/sankt-augustin/" data-label_1="SANKT AUGUSTIN" data-label_2="Wetter" data-mode="Current" data-days="3" data-theme="weather_one" >SANKT AUGUSTIN Wetter</a>
+<a class="weatherwidget-io" href="https://forecast7.com/en/50d777d19/sankt-augustin/" data-label_1="SANKT AUGUSTIN" data-label_2="Wetter" data-mode="Current" data-days="3" data-theme="weather_one" >SANKT AUGUSTIN Wetter</a>
 <script>
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 </script>
