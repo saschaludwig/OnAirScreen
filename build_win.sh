ca849345848776ffb3233a515d2ca7944ff2f9a0
Sascha Ludwig <sascha@rc5.de>
2020-04-24 00:14:32 +0200

    fixed icon and build_win

--- build_win.sh
+++ build_win.sh
@@ -3,7 +3,7 @@ rm -rf ./dist/win/*
 source venv/bin/activate
 make && \
 eval $(docker-machine env default) && \
-docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pip install -r requirements.txt && pyinstaller -F -y -w --clean -i images/oas_icon.ico --dist ./dist/win --workpath /tmp --specpath /tmp -n OnAirScreen start.py ; pyinstaller -F -y -c --clean --dist ./dist/win --workpath /tmp --specpath /tmp -n oas_send utils/oas_send.py" && \
+docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pip install -r requirements.txt && cp -v images/oas_icon.ico /tmp && pyinstaller -F --clean -y -w --icon='oas_icon.ico' --dist ./dist/win --workpath /tmp --specpath /tmp -n OnAirScreen start.py ; pyinstaller -F -y --clean -c --dist ./dist/win --workpath /tmp --specpath /tmp -n oas_send utils/oas_send.py" && \
 mv -f ./dist/win/OnAirScreen.exe ./dist/win/OnAirScreen_$(git describe --abbrev=0 --tags)_$(git rev-parse --short HEAD).exe
 mv -f ./dist/win/oas_send.exe ./dist/win/oas_send_$(git describe --abbrev=0 --tags)_$(git rev-parse --short HEAD).exe
 
